22363dbc64128d93744438a6b43cbd9727993eed
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-23 22:28:24 -0300

    feat: mongoDb application service

---
+++ Application/Services/MongoDbService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace myWorkes.Application.Services
+{
+	public class MongoDbService
+	{
+		private IConfiguration _configuration;
+		private IMongoDatabase _database;
+
+		public MongoDbService(IConfiguration _configuration)
+		{
+			this._configuration = _configuration;
+
+			var connectionString = _configuration.GetConnectionString("DbConnection");
+			var mongoUri = MongoUrl.Create(connectionString);
+			var mongoClient = new MongoClient(mongoUri);
+			_database = mongoClient.GetDatabase(mongoUri.DatabaseName);
+		}
+
+		public IMongoDatabase? Database => _database;
+	}
+}
+
