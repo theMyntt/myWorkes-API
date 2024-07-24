ae70182f0fff670de222f5538b594b90ccfa8093
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-24 14:17:23 -0300

    chore: employee entity mapper

---
+++ Infrastructure/Repositories/EmployeeMapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using myWorkes.Core;
+using myWorkes.Domain.Aggregates;
+using myWorkes.Infrastructure.Schema;
+
+namespace myWorkes.Infrastructure.Repositories
+{
+	public class EmployeeMapper : IMapperService<EmployeeAggregate, Employee>
+	{
+        public EmployeeAggregate toDomain(Employee schema)
+        {
+            return new EmployeeAggregate(schema.Id, schema.Name, schema.Email, schema.Phone, schema.Password, schema.Type, schema.AllowedSystems, schema.CreatedAt, schema.UpdatedAt);
+        }
+
+        public Employee toPersistance(EmployeeAggregate schema)
+        {
+            return new Employee
+            {
+                Id = schema.Id,
+                Name = schema.Name,
+                Email = schema.Email,
+                Phone = schema.Phone,
+                Password = schema.Password,
+                Type = schema.Type,
+                AllowedSystems = schema.AllowedSystems,
+                CreatedAt = schema.CreatedAt,
+                UpdatedAt = schema.UpdatedAt
+            };
+        }
+    }
+}
+
