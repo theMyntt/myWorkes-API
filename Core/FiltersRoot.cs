052a08946a0832631395c25b9ed669b54c4210e5
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-24 14:10:27 -0300

    chore: repository interface contract

---
+++ Core/FiltersRoot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace myWorkes.Core
+{
+	public class FiltersRoot
+	{
+		public required string Field { get; set; }
+		public required string Value { get; set; }
+
+		public FiltersRoot() { }
+
+		public FiltersRoot(string Field, string Value)
+		{
+			this.Field = Field;
+			this.Value = Value;
+		}
+	}
+}
+
---
+++ Core/IRepositoryService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using myWorkes.Domain.Aggregates;
+
+namespace myWorkes.Core
+{
+	public interface IRepositoryService
+	{
+		Task<ActionResult<IEnumerable<EmployeeAggregate>>> Filter(List<FiltersRoot> filters, int page);
+		Task<ActionResult<EmployeeAggregate>> Find(List<FiltersRoot> filters);
+		Task<ActionResult<StandardResponse>> Create(EmployeeAggregate employee);
+		Task<ActionResult<StandardResponse>> Edit(EmployeeAggregate employee);
+		Task<ActionResult<StandardResponse>> Delete(EmployeeAggregate employee);
+	}
+}
+
