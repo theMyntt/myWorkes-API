03a662aa73aa81088541de3459f8b29f9704056a
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-24 14:00:54 -0300

    feat: base employee entities

---
+++ Domain/Aggregates/EmployeeAggregate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using myWorkes.Application.Utils;
+using myWorkes.Core;
+using myWorkes.Domain.Enums;
+
+namespace myWorkes.Domain.Aggregates
+{
+	public class EmployeeAggregate : AggregateRoot 
+	{
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Phone { get; set; }
+        public required string Password { get; set; }
+        public required EEmployeerType Type { get; set; }
+        public required List<string>? AllowedSystems { get; set; }
+
+        public EmployeeAggregate(string Name, string Email, string? Phone, string Password, EEmployeerType Type, List<string>? AllowedSystems)
+		{
+            Id = UUID.generate();
+            this.Name = Name;
+            this.Email = Email;
+            this.Phone = Phone;
+            this.Password = Password;
+            this.Type = Type;
+            this.AllowedSystems = AllowedSystems;
+            CreatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now;
+		}
+
+        public EmployeeAggregate(string Id, string Name, string Email, string? Phone, string Password, EEmployeerType Type, List<string>? AllowedSystems, DateTime CreatedAt)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Email = Email;
+            this.Phone = Phone;
+            this.Password = Password;
+            this.Type = Type;
+            this.AllowedSystems = AllowedSystems;
+            this.CreatedAt = CreatedAt;
+            UpdatedAt = DateTime.Now;
+        }
+
+        public EmployeeAggregate(string Id, string Name, string Email, string? Phone, string Password, EEmployeerType Type, List<string>? AllowedSystems, DateTime CreatedAt, DateTime UpdatedAt)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Email = Email;
+            this.Phone = Phone;
+            this.Password = Password;
+            this.Type = Type;
+            this.AllowedSystems = AllowedSystems;
+            this.CreatedAt = CreatedAt;
+            this.UpdatedAt = UpdatedAt;
+        }
+	}
+}
+
---
+++ Infrastructure/Schema/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using myWorkes.Core;
+using myWorkes.Domain.Enums;
+
+namespace myWorkes.Infrastructure.Schema
+{
+	public class Employee : EntityRoot
+	{
+		[BsonElement("name")]
+		[BsonRepresentation(BsonType.String)]
+		public required string Name { get; set; }
+
+        [BsonElement("email")]
+        [BsonRepresentation(BsonType.String)]
+        public required string Email { get; set; }
+
+        [BsonElement("phone")]
+        [BsonRepresentation(BsonType.String)]
+        public string? Phone { get; set; }
+
+        [BsonElement("password")]
+        [BsonRepresentation(BsonType.String)]
+        public required string Password { get; set; }
+
+        [BsonElement("type")]
+        [BsonRepresentation(BsonType.String)]
+        public required EEmployeerType Type { get; set; }
+
+        [BsonElement("allowedSystems")]
+        [BsonRepresentation(BsonType.Array)]
+        public List<string>? AllowedSystems { get; set; }
+    }
+}
+
